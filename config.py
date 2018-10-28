b372bd5791e1e4018061362b20b3e8adb0aeb4f1
vdrezy <agamerto116@gmail.com>
2018-10-27 21:00:55 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+token = 'NTAwMDkwNDkwMTgwNDY4NzM2.DrAg5A.juQyTCz1E4vn_2oExkfbBt2giFI'
+
+prefix = '..' #put prefix here
+
+link = 'https://discordapp.com/api/oauth2/authorize?client_id=500090490180468736&permissions=8&scope=bot' #put bot invite link here
+
+ownerid = '104001063148879872' #put your id here
